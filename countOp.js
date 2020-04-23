77507ee4208678890691ef6aa92dde06e94dabb6
TorrieL <hmminicity2@163.com>
2020-04-23 13:28:44 +0800

    Big O Notation: Counting Operations

--- countOp.js
+++ countOp.js
@@ -11,3 +11,17 @@ const sumupFor = n => {
   }
   return total
 }
+
+let time1,time2
+// let arg = 1990021519820106
+let arg = 10000000000
+
+time1 = Date.now();
+sumUpTo(arg)
+time2 = Date.now();
+console.log(`sumUpTo() uses ${(time2 - time1) / 1000} seconds to count ${arg}`) // 0sec
+
+time1 = Date.now();
+sumupFor(arg)
+time2 = Date.now();
+console.log(`sumupFor() uses ${(time2 - time1) / 1000} seconds to count ${arg}`) // 11.288 sec
