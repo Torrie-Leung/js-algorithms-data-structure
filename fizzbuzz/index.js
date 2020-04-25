bc2f60d37bfc1bd1f7c9c55545124d2bd8ee6df8
TorrieL <hmminicity2@163.com>
2020-04-25 20:34:51 +0800

    fizzbuzz solution 2

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,17 +12,31 @@
 //   buzz
 
 function fizzBuzz(n) {
+  // solution 1
+  // for(let i = 1; i <= n; i++){
+  //   if(i % 3 === 0 && i % 5 === 0 ){
+  //     console.log('fizzbuzz')
+  //   }else if(i % 3 === 0){
+  //     console.log('fizz')
+  //   }else if(i % 5 === 0){
+  //     console.log('buzz')
+  //   }else{
+  //     console.log(i)
+  //   }
+  // }
+  // solution 2
   for(let i = 1; i <= n; i++){
-    if(i % 3 === 0 && i % 5 === 0 ){
-      console.log('fizzbuzz')
-    }else if(i % 3 === 0){
-      console.log('fizz')
-    }else if(i % 5 === 0){
-      console.log('buzz')
-    }else{
-      console.log(i)
+    let str = ''
+    if(i % 3 === 0){
+      str += 'fizz'
     }
-    
+    if(i % 5 === 0){
+      str += 'buzz'
+    }
+    if(str === ''){
+      str = i
+    }
+    console.log(str)
   }
 }
 
