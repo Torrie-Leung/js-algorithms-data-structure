b50f1939a6087da66f728d26ebcf8081ae354cdc
TorrieL <hmminicity2@163.com>
2020-04-28 22:54:40 +0800

    main diffs between foreach and foloop

--- Array-Methods-Mini-Course/forEach.js
+++ Array-Methods-Mini-Course/forEach.js
@@ -5,10 +5,10 @@ const colors = ['red','green','blue'];
 // }
 
 // if you using it a lot name it otherwise anonymou arrowfunc is ok
-function clgColor(color){
-  console.log(color);
-}
-colors.forEach(clgColor)
+// function clgColor(color){
+//   console.log(color);
+// }
+// colors.forEach(clgColor)
 
 colors.forEach(color => console.log(color))
 
@@ -16,3 +16,17 @@ const nums = [1,2,3]
 const doubledNums = []
 nums.forEach(num => doubledNums.push(num*2))
 console.log(doubledNums);
+
+const n = [1,2,1,1,1,1,1,]
+let hasEven = false
+
+// for loops do not require an existing array to run
+// you can only use a break statement inside a for/while loop
+for(let i = 0; i < n.length; i++){
+  console.log(`this for loop has ran ${i + 1} times`)
+  if(n[i] % 2 == 0){
+    hasEven = true
+    break
+  }
+}
+console.log(hasEven);
