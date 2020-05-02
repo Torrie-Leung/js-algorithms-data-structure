995f01eb348c3ad6eff7ced9fa43230c03c80fb3
TorrieL <hmminicity2@163.com>
2020-05-02 19:26:02 +0800

    more filter basic

--- Array-Methods-Mini-Course/filter.js
+++ Array-Methods-Mini-Course/filter.js
@@ -2,12 +2,19 @@ const squad = [
   {name: 'Mario', born: 1975, gender: 'male', income: 23000},
   {name: 'Kuba', born: 1965, gender: 'male', income: 25000},
   {name: 'Luigi', born: 1995, gender: 'male', income: 13000},
-  {name: 'Peach', born: 1985, gender: 'female', income: 230000},
+  {name: 'Peach', born: 1989, gender: 'female', income: 230000},
+  {name: 'Toadette', born: 1999, gender: 'female', income: 170000},
   {name: 'Gnome', born: 2000, gender: 'male', income: 23000},
 ];
 
-const agedRichMale = squad.filter(member => {
+const agedRichGuys = squad.filter(member => {
   const age = new Date().getFullYear() - member.born;
   return age >= 40 && member.income > 10000
 })
-console.log(agedRichMale);
+console.log(agedRichGuys);
+
+const youngRichLadies = squad.filter(member => {
+  const age = new Date().getFullYear() - member.born;
+  return age <= 40 && member.income > 10000 && member.gender === 'female'
+})
+console.log(youngRichLadies);
