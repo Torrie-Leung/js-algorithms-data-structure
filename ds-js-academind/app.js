c1e3d4dc43b0fc70d113df5477bfb463c8422c5c
TorrieL <hmminicity2@163.com>
2020-08-09 11:52:22 +0800

    set value to map

--- ds-js-academind/app.js
+++ ds-js-academind/app.js
@@ -1,3 +1,4 @@
+// array
 const names = ['Daveed', 'Lin', 'Reenem', 'Mario', 'Mario'];
 
 for( const el of names){
@@ -5,4 +6,31 @@ for( const el of names){
 }
 
 const marioIndex = names.findIndex(el => el === 'Mario')
-console.log(marioIndex);
+console.log({marioIndex});
+
+//set
+const ids = new Set();
+
+ids.add('jimmy')
+ids.add(1)
+ids.add('bb2')
+ids.add(1)
+for( const el of ids){
+  console.log(el);
+}
+console.log(ids.has('bb2'));
+ids.delete('bb2')
+console.log(ids.has('bb2'));
+
+// map
+const resultData = new Map()
+resultData.set('average', 1.62)
+resultData.set('lastResult', null)
+
+const germany = {name: 'Germany', population: 82}
+
+resultData.set(germany, 0.89)
+resultData.set('average', 0.62) // overwrite the same first one
+for(const el of resultData){
+  console.log(el);
+}
